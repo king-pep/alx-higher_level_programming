afc1d025e9dfce906f2ac23a97e0145fee6dac76
Abel Morris Ben <abelmorrisben@icloud.com>
2022-12-04 00:33:16 -0800

    no c 2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_str = ""
+    new_string = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
+        if i != 'c' and i != 'C':
+            new_string += i
+    return new_string
