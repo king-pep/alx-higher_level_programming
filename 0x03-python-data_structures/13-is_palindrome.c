27af2b17f536cb2673697ae01505c03f6dc17dc6
Abel Morris Ben <abelmorrisben@icloud.com>
2022-12-04 00:51:04 -0800

    palindrome 3

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-* list_len - finds no. of elements ina linked list.
-* @h: pointer to linked list.
-*
-* Return: number of elements in linked list.
-*/
-size_t list_len(listint_t *h)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	size_t  nodes = 0;
+	listint_t *head;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-
-/**
-* is_palindrome - checks if a singly linked list is a palindrome.
-* @head: double pointert to head of d-list.
-*
-* Return: 1 if palindrome, 0 otherwise.
-*/
-size_t list_len(const listint_t *h)
-{
-    size_t i = 0;
-
-    while (h)
-    {
-        i++;
-        h = h->next;
-    }
-    return (i);
+	return (0);
 }
