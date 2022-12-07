659f4a72c809da6f675dfe8dd7f82e269174a681
Abel Morris Ben <abelmorrisben@icloud.com>
2022-12-07 11:16:57 -0800

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i in matrix:
-        new_matrix.append(list(map(lambda x: x ** 2, i)))
-    return new_matrix
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+    return (tmp)
