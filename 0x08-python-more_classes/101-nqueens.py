8c211483e6fb0e8455acb238bae5baa350e6b468
Abel Ben <abelmorrisben@icloud.com>
2023-01-04 05:03:53 +0000

    1-1 #454

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,81 +1,50 @@
 #!/usr/bin/python3
-"""Module is to solve the N-Queens challenge problem"""
-from sys import argv
+import sys
 
 
-def checkspot(board, r, c):
-    n = len(board) - 1
-    if board[r][c]:
-        return 0
-    for row in range(r):
-        if board[row][c]:
-            return 0
-    i = r
-    j = c
-    while i > 0 and j > 0:
-        i -= 1
-        j -= 1
-        if board[i][j]:
-            return 0
-    i = r
-    j = c
-    while i > 0 and j < n:
-        i -= 1
-        j += 1
-        if board[i][j]:
-            return 0
-    return 1
+def nqueens(n):
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    if type(n) != int:
+        print("N must be a number")
+        sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    board = []
+    for i in range(n):
+        board.append([])
+        for j in range(n):
+            board[i].append(0)
+    solve(board, 0, n)
 
 
-def initboard(n=4):
-    b = []
-    for r in range(n):
-        b.append([0 for c in range(n)])
-    return b
+def solve(board, col, n):
+    if col >= n:
+        print(board)
+        return True
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve(board, col + 1, n) or res
+            board[i][col] = 0
+    return res
 
 
-def findsoln(board, row):
-    for col in range(len(board)):
-        if checkspot(board, row, col):
-            board[row][col] = 1
-            if row == len(board) - 1:
-                print(convtosoln(board))
-                board[row][col] = 0
-                continue
-            if findsoln(board, row + 1):
-                return board
-            else:
-                board[row][col] = 0
-    return None
-
-
-def convtosoln(board):
-    soln = []
-    n = len(board)
-    for r in range(n):
-        for c in range(n):
-            if board[r][c]:
-                soln.append([r, c])
-    return soln
-
-
-def nqueens(n=4):
-    for col in range(n):
-        board = initboard(n)
-        board[0][col] = 1
-        findsoln(board, 1)
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    try:
-        n = int(argv[1])
-    except:
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-    nqueens(n)
+    nqueens(int(sys.argv[1]))
