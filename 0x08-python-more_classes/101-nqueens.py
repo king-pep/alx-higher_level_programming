bf14300c8fcbf8edca83f4958f72bbe96894c440
Abel Ben <abelmorrisben@icloud.com>
2023-01-04 05:28:17 +0000

    101 #8

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,33 +3,33 @@
 import sys
 
 
-def nqueens(p):
-    if p < 4:
+def nqueens(n):
+    if n < 4:
         print("N must be at least 4")
         return
-    board = [[0 for i in range(p)] for j in range(p)]
-    solve(board, 0, p)
+    board = [[0 for i in range(n)] for j in range(n)]
+    solve(board, 0, n)
 
 
-def solve(board, col, p):
-    if col >= p:
+def solve(board, col, n):
+    if col >= n:
         print(board)
         return
-    for i in range(p):
-        if isSafe(board, i, col, p):
+    for i in range(n):
+        if isSafe(board, i, col, n):
             board[i][col] = 1
-            solve(board, col + 1, p)
+            solve(board, col + 1, n)
             board[i][col] = 0
 
 
-def isSafe(board, row, col, p):
+def isSafe(board, row, col, n):
     for i in range(col):
         if board[row][i] == 1:
             return False
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-    for i, j in zip(range(row, p, 1), range(col, -1, -1)):
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
     return True
