f41a3e6706d7c1e6464855198a7ff356c045ff53
Abel Ben <abelmorrisben@icloud.com>
2023-01-07 12:19:24 +0000

    nqueen #9

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,47 +1,64 @@
 #!/usr/bin/python3
+"""N Queens Module.
 
+Contains the N Queens problem solver.
+"""
 import sys
 
 
-def nqueens(n):
-    if n < 4:
-        print("N must be at least 4")
-        return
-    board = [[0 for i in range(n)] for j in range(n)]
-    solve(board, 0, n)
+def error_exit(message="", code=1):
+    """Handles exit.
+
+    Args:
+        message (str): the message to display on stdout.
+        code (int): the exit code.
+    """
+    print(message)
+    exit(code)
 
 
-def solve(board, col, n):
-    if col >= n:
-        print(board)
-        return
-    for i in range(n):
-        if isSafe(board, i, col, n):
-            board[i][col] = 1
-            solve(board, col + 1, n)
-            board[i][col] = 0
+def test_pos(board, y):
+    """Tests if wether a queen can be placed at the current position.
 
-
-def isSafe(board, row, col, n):
-    for i in range(col):
-        if board[row][i] == 1:
+    Args:
+        board (list): the chessboard.
+        y (int): the height parameter.
+    """
+    for i in range(y):
+        if board[y][1] is board[i][1]:
             return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
+        if abs(board[y][1] - board[i][1]) == y - i:
             return False
     return True
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except:
-        print("N must be a number")
-        sys.exit(1)
-    nqueens(n)
+def rec_backtrack(board, y):
+    """Backtrack the possibilities.
+
+    Args:
+        board (list): the chessboard.
+        y (int): the height parameter.
+    """
+    if y is N:
+        print(board)
+    else:
+        for x in range(N):
+            board[y][1] = x
+            if test_pos(board, y):
+                rec_backtrack(board, y + 1)
+
+
+if len(sys.argv) is not 2:
+    error_exit("Usage: nqueens N")
+
+try:
+    N = int(sys.argv[1])
+except:
+    error_exit("N must be a number")
+
+if N < 4:
+    error_exit("N must be at least 4")
+
+board = [[y, 0] for y in range(N)]
+rec_backtrack(board, 0)
+
