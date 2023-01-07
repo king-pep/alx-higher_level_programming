aa8415656011d7ff99a4a7835a1bde22189e4c19
Abel Ben <abelmorrisben@icloud.com>
2023-01-07 12:35:02 +0000

    nqueen #10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,75 +1,58 @@
 #!/usr/bin/python3
-
-"""Solves the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-Example:
-    $ ./101-nqueens.py N
-N must be an integer greater than or equal to 4.
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
-
 import sys
 
-board = []
-solutions = []
 
-
-def isSafe(row, col):
-    for i in range(col):
-        if board[row][i]:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    return True
-
-
-def solveNQUtil(col):
-    if col >= len(board):
-        solutions.append([i for i in board])
+def nqueens(n):
+    def is_safe(board, row, col):
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
         return True
 
-    res = False
-    for i in range(len(board)):
-        if isSafe(i, col):
-            board[i][col] = 1
-            res = solveNQUtil(col + 1) or res
-            board[i][col] = 0
+    def solve(board, col):
+        if col >= n:
+            return True
+        for i in range(n):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+                if solve(board, col + 1):
+                    return True
+                board[i][col] = 0
+        return False
 
-    return res
+    board = [[0 for j in range(n)] for i in range(n)]
+    if not solve(board, 0):
+        return False
+    return board
 
 
-def solveNQ():
-    if not solveNQUtil(0):
-        print("No solution exists")
-
-    for solution in solutions:
-        print([[row, col] for col in range(len(board)) for row in range(len(board)) if solution[row][col]])
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = nqueens(n)
+    if not board:
+        return
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                print([i, j], end=" ")
+        print()
 
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-    N = int(sys.argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-    board = [[0 for i in range(N)] for j in range(N)]
-    solveNQ()
+    main()
